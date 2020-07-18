b73a9bc11ce0ca706f409d95665592a297a6590a
김경민 | KimKyeongMin <am030927@naver.com>
2020-07-19 00:46:32 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -4,8 +4,11 @@ const clockTitle = clockContainer.querySelector("h1");
 function getTime(){
     const date = new Date();
     const minutes = date.getMinutes();
-    const hours = date.getHours();
+    var hours = date.getHours();
     const seconds = date.getSeconds();
+    if hours == 0{
+    hours = 12;
+    }
     clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
 function init(){
